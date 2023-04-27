23fe1ac44520fa1644b87fc9292cfc8edd341f81
Guilherme Fontes <gui.fontes.amorim@gmail.com>
2023-04-27 16:32:00 -0400

    feat: add prisma with default schema

--- src/sdk/server/context.ts
+++ src/sdk/server/context.ts
@@ -1,11 +1,15 @@
 import * as trpc from '@trpc/server'
 import * as trpcNext from '@trpc/server/adapters/next'
+import prisma from '@/sdk/lib/prisma'
 
 export const createContext = async (
   opts?: trpcNext.CreateNextContextOptions,
 ) => {
   return {
     req: opts?.req,
+    clients: {
+      prisma,
+    },
   }
 }
 
